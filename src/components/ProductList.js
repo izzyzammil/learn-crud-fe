31d09d4b4a01bf46286834ceb452eca315dc95a7
Izzy Zammil <izzy.zammil@gmail.com>
2022-10-31 10:31:17 +0700

    add productList in components

---
+++ src/components/ProductList.js
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const deleteProduct = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:5000/products/${productId}`);
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <Link to="/add" className="button is-success">
+        Add New
+      </Link>
+
+      <div className="columns is-multiline mt-2">
+        {products.map((product) => (
+          <div className="column is-one-quarter" key={product.id}>
+            <div className="card">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={product.url} alt="image" />
+                </figure>
+              </div>
+
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="title is-4">{product.name}</p>
+                  </div>
+                </div>
+              </div>
+
+              <footer className="card-footer">
+                <Link to={`edit/${product.id}`} className="card-footer-item">
+                  Edit
+                </Link>
+                <a
+                  onClick={() => deleteProduct(product.id)}
+                  className="card-footer-item"
+                >
+                  Delete
+                </a>
+              </footer>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
